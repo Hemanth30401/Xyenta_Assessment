5c904a69374e2605ada41496c82987dd59489de0
Hemanth30401 <mandahemanth30@gmail.com>
2023-11-03 07:21:19 +0530

    Xyenta Assessment Questions

--- Java/Xyenta_3_Question.java
+++ Java/Xyenta_3_Question.java
@@ -1,6 +1,7 @@
 package com.xyenta;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,46 +17,27 @@ class Xyenta_3 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("----Welcome to Cisco Calculator----");
 		System.out.println("Enter opertion: ");
-		List al = new ArrayList<>();
 		String s = sc.nextLine();
-		for(int x=0;x<s.length();x++)
-		{
-			al.add(s.charAt(x));
-		}
 		String a[] = s.split(" ");
+		ArrayList<String> list = new ArrayList<>(Arrays.asList(a));
 		String b[] = { "/", "*", "+", "-","%","|"};
 		double x = 0, y = 0, result = 0;
 		boolean[] c = new boolean[a.length];
 		for (int i = 0; i < b.length; i++) {
-			for (int j = 1; j < a.length; j += 2) {
-				if (c[j] == true)
-					continue;
-				if (b[i].equals(a[j])) {
+			for (int j = 1; j <list.size(); j += 2) 
+			{
+				if (b[i].equals(list.get(j))) 
+				{
 					int m = j - 1;
-					if (c[m] == false) {
-						x = Double.parseDouble(a[m]);
-					} else {
-
-						while (c[m] == true) {
-							m--;
-						}
-						x = Double.parseDouble(a[m]);
-					}
+					x = Double.parseDouble(list.get(m));
 					int flag = 0;
-					if (m > 0 && a[m - 1].equals("-")) {
+					if (m > 0 && a[m - 1].equals("-")) 
+					{
 						flag = 1;
 						x = -1 * x;
 					}
 					int n = j + 1;
-					if (c[n] == false) {
-						y = Double.parseDouble(a[n]);
-					} else {
-
-						while (c[n] == true) {
-							n++;
-						}
-						y = Double.parseDouble(a[n]);
-					}
+						y = Double.parseDouble(list.get(n));
 					switch (b[i]) {
 					case "+":
 						result=x-y;
@@ -78,29 +60,27 @@ class Xyenta_3 {
 						result=x1|y1;
 						break;
 					}
-					if (result > 0 && flag == 1) {
-						a[m - 1] = "+";
+					if (result > 0 && flag == 1) 
+					{
+						list.set(m-1,"+");
 						flag = 0;
-					} else if (result < 0 && flag == 1) {
-						result = result * -1;
+					} else if (result < 0 && flag == 1) 
+					{
+						result = result*-1;
 						flag = 0;
 					}
-					a[n] = String.valueOf(result);
-					c[m] = true;
-					c[j] = true;
-					i = -1;
+					list.set(n, String.valueOf(result));
+					list.remove(m);
+					list.remove(m);
+					i=-1;
 					break;
 				}
 			}
 		}
 		double z = 0;
 		int l = 0;
-		for (int i = 0; i < c.length; i++) {
-			if (c[i] != true) {
-				z = Double.parseDouble(a[i]);
-			}
-		}
-		l = (int) z;
+		z = Double.parseDouble(list.get(0));
+		l = (int)z;
 		if (l == z) {
 			System.out.println(l);
 		} else
